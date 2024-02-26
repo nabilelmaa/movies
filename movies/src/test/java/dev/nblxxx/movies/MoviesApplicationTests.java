95b85e2dc969dd9b0adb1cc1e577b0790fb395c1
Nabil El Maaroufi <93434382+nabilelmaa@users.noreply.github.com>
2024-02-26 21:45:40 +0100

    first commit

---
+++ movies/src/test/java/dev/nblxxx/movies/MoviesApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.nblxxx.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
